8f7adfff673577e1a9429f23b041f3393e8418b9
HarryTr <hung.tv@hanbiro.com>
2021-09-12 09:10:42 +0700

    Update sources

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import vue component
+import DateTimePicker from "./components/DateTimePicker.vue";
+import DateTimePickerModal from "./components/DateTimePickerModal.vue";
+import Calender from "./components/DatePicker/Calender.vue";
+import TimePicker from "./components/TimePicker";
+import DatePicker from "./components/DatePicker";
+
+export default DateTimePicker;
+export {
+    DatePicker,
+    TimePicker,
+    Calender,
+    DateTimePickerModal
+}
